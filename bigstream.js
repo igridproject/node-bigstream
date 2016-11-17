a24a928018c9d4b37f962079632d16505dc62cf5
project <project@4391fc91-8685-4224-b3a7-50582221be54>
2016-11-17 08:22:57 +0000

    gcs

---
+++ bigstream.js
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain; charset=UTF-8'
+    });
+    
+    res.end('Hello from node-bigstream.\n');
+    
+}).listen(9080, "");
