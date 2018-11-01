ce64e3c7e22ed7375514082a0eae3424aa620b10
Pongsakorn Udombua <udombua@kkumail.com>
2018-11-01 15:49:31 +0700

    Add memstore setHash and memstore with out prefix

--- jobworker/lib/memstore.js
+++ jobworker/lib/memstore.js
@@ -17,9 +17,44 @@ memstore.prototype.setItem = function(k,v,cb){
   this.mem.set(key,value,cb);
 }
 
+memstore.prototype.setItemKey = function(k,v,cb){
+  var key = k;
+  var value = JSON.stringify(v);
+  this.mem.set(key,value,cb);
+}
+
+memstore.prototype.setHash = function(h,k,v,cb){
+  var key = this.prefix + ":" + k;
+  var value = JSON.stringify(v);
+  this.mem.hset(h,key,value,cb);
+}
+
+memstore.prototype.setHashKey = function(h,k,v,cb){
+  var key = k;
+  var value = JSON.stringify(v);
+  this.mem.hset(h,key,value,cb);
+}
+
 memstore.prototype.getItem = function(k,cb)
 {
   var key = this.prefix + ":" + k;
+  this.mem.get(key,function(err,v){
+    var value = null;
+    if(!err && v){
+      if(typeof v == 'object' && v.type == 'Buffer')
+      {
+        value = new Buffer(v.data);
+      }else{
+        value = JSON.parse(v);
+      }
+    }
+    cb(err,value);
+  });
+}
+
+memstore.prototype.getItemKey = function(k,cb)
+{
+  var key = k;
   this.mem.get(key,function(err,v){
     var value = null;
     if(!err && v){
