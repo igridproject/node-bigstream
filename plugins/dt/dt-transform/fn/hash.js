49c5fdeee61fe09bc49fb3450aa9d0e39cc8fe1b
Kamron Aroonrua <gookcompsci@hotmail.com>
2020-05-14 19:08:17 +0700

    hash

--- plugins/dt/dt-transform/fn/hash.js
+++ plugins/dt/dt-transform/fn/hash.js
@@ -1,5 +1,6 @@
 var crypto = require('crypto');
 
 module.exports.sha256 = function (text) {
-    return crypto.createHash('sha256').update(text).digest('hex');
+    var dat = (typeof text == 'string')?text:String(text);
+    return crypto.createHash('sha256').update(dat).digest('hex');
 }
